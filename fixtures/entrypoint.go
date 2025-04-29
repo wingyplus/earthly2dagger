96d95b18ee5402c0053fd8472b51a696aac76142
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2025-04-30 00:02:32 +0700

    fix: add forgot fixture files
    Signed-off-by: Thanabodee Charoenpiriyakij <wingyminus@gmail.com>

---
+++ fixtures/entrypoint.go
@@ -0,0 +1,29 @@
+package main
+
+import "dagger/my-module/internal/dagger"
+
+type MyModule struct {
+	Container *dagger.Container
+}
+
+func New(
+	// +optional
+	container *dagger.Container,
+) *MyModule {
+	if container == nil {
+		container = dag.Container()
+	}
+	return &MyModule{Container: container}
+}
+
+func (m *MyModule) TestEntrypointString() *dagger.Container {
+	return m.Container.
+		From("alpine").
+		WithEntrypoint([]string{"sh", "-c", `echo 'hello, world'`})
+}
+
+func (m *MyModule) TestEntrypointArray() *dagger.Container {
+	return m.Container.
+		From("alpine").
+		WithEntrypoint([]string{"echo", "hello, world"})
+}
